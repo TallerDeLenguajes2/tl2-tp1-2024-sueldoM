97420bf3d233e600b6507763180309f4719fcf7c
unknown <sueldo.011.ms@gmail.com>
2024-08-25 22:44:08 -0300

    10th commit

--- TP1/cadeteria.cs
+++ TP1/cadeteria.cs
@@ -6,4 +6,32 @@ public class Cadeteria{
     private Cadeteria(){
         ListadodeCadetes = new List<Cadete>();
     }
+
+    public void AgregarCadete(Cadete cadete){
+        ListadodeCadetes.Add(cadete);
+    }
+
+    public void removerCadete(int id){
+        var cadete = ListadodeCadetes.FirstOrDefault(c => c.Id == id);
+        if (cadete != null)
+        {
+            ListadodeCadetes.Remove(cadete);
+        }
+    }
+
+    public Cadete BuscarCadete(int id){
+        return ListadodeCadetes.FirstOrDefault(c => c.Id == id);
+    }
+
+    public void reasignarPedido(int nroPedido, int idCadeteOriginal, int idCadeteNuevo){
+        var cadeteOriginal = BuscarCadete(idCadeteOriginal);
+        var cadeteNuevo = BuscarCadete(idCadeteNuevo);
+        var pedido = cadeteOriginal?.ListadodePedidos.FirstOrDefault(p => p.Nro == nroPedido);
+
+        if (pedido != null && cadeteNuevo != null){
+            cadeteOriginal.reasignarPedido(pedido, cadeteNuevo);
+        }
+    }
+
+    
 }
